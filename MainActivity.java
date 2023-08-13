4ffa4bd83fcdafa8621a37b6aec612f84474fbb5
Ali Ferzali <Ali.ferzali@gmail.com>
2023-08-13 23:38:19 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,17 +2,20 @@ package com.example.encryption;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Base64;
+
+import javax.crypto.SecretKey;
+
 
 public class MainActivity extends AppCompatActivity {
     private EditText inputMessage;
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Koppla XML-elementer till Java-variabler
         inputMessage = findViewById(R.id.inputMessage);
         ciphEdt = findViewById(R.id.ciphEdt);
         key_dt = findViewById(R.id.key_dt);
@@ -41,73 +45,95 @@ public class MainActivity extends AppCompatActivity {
         tV1 = findViewById(R.id.tV1);
 
         try {
+            // Lyssnare för krypteringsknapp
             btnEncrypt.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     String originalText = inputMessage.getText().toString().trim();
                     String cipherText = ciphEdt.getText().toString().trim();
-                    String keyLength = key_dt.getText().toString().trim();
+                    String keyLengthStr = key_dt.getText().toString().trim();
                     String selectedAlgorithm = algorithm.getText().toString().trim();
 
-                    if (!originalText.isEmpty() && !keyLength.isEmpty() && !selectedAlgorithm.isEmpty()) {
+                    // Kontrollera att användaren har fyllt i alla fält
+                    if (!originalText.isEmpty() && !keyLengthStr.isEmpty() && !selectedAlgorithm.isEmpty()) {
                         try {
+                            int keyLength = Integer.parseInt(keyLengthStr);
+
+                            // Kryptering med AES
                             if (selectedAlgorithm.equalsIgnoreCase("AES")) {
-                                // Generera AES-nyckel
-                                aesKey = EncryptionUtils.generateAESKey(Integer.parseInt(keyLength));
-
-                                // Kryptera med AES
-                                String encryptedText = EncryptionUtils.encryptAES(originalText, aesKey);
-                                tV1.setText(encryptedText);
-                            } else if (selectedAlgorithm.equalsIgnoreCase("RSA")) {
-                                // Generera RSA-nyckelpar
-                                KeyPair rsaKeyPair = EncryptionUtils.generateRSAKeyPair(Integer.parseInt(keyLength));
-                                publicKey = rsaKeyPair.getPublic();
-                                privateKey = rsaKeyPair.getPrivate();
-
-                                // Kryptera med RSA
-                                String encryptedText = EncryptionUtils.encryptRSA(originalText, publicKey);
-                                tV1.setText(encryptedText);
-                            } else {
-                                Toast.makeText(MainActivity.this, "Ogiltig/fel algoritm har valts", Toast.LENGTH_SHORT).show();
+                                aesKey = EncryptionUtils.generateAESKey(keyLength);
+                                if (!originalText.equals(cipherText)) {
+                                    cipherText = EncryptionUtils.encryptAES(originalText, aesKey);
+                                    ciphEdt.setText(cipherText);
+                                }
                             }
+                            // Kryptering med RSA
+                            else if (selectedAlgorithm.equalsIgnoreCase("RSA")) {
+                                if (keyLength == 128 || keyLength == 256) {
+                                    KeyPair rsaKeyPair = EncryptionUtils.generateRSAKeyPair(keyLength);
+                                    publicKey = rsaKeyPair.getPublic();
+                                    privateKey = rsaKeyPair.getPrivate();
+                                    cipherText = EncryptionUtils.encryptRSA(originalText, publicKey);
+                                    ciphEdt.setText(cipherText);
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Invalid RSA Key Length", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(MainActivity.this, "Invalid/Incorrect Algorithm selected", Toast.LENGTH_SHORT).show();
+                            }
+                           // Validation/Error check av inputs från användare
+                            tV1.setText("Encrypted Text: " + cipherText);
+                        } catch (NumberFormatException e) {
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Invalid Key Length", Toast.LENGTH_SHORT).show();
                         } catch (Exception e) {
                             e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Encryption failed", Toast.LENGTH_SHORT).show();
                         }
                     } else {
-                        Toast.makeText(MainActivity.this, "Fyll i alla fälten", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Fill in all fields", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
 
+            // Lyssnare för dekrypteringsknapp
             btnDecrypt.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    String originalText = inputMessage.getText().toString().trim();
                     String cipherText = ciphEdt.getText().toString().trim();
-                    String keyLength = key_dt.getText().toString().trim();
+                    String keyLengthStr = key_dt.getText().toString().trim();
                     String selectedAlgorithm = algorithm.getText().toString().trim();
 
-                    if (!cipherText.isEmpty() && !keyLength.isEmpty() && !selectedAlgorithm.isEmpty()) {
+                    // Kontrollera att användaren har fyllt i alla fält
+                    if (!cipherText.isEmpty() && !keyLengthStr.isEmpty() && !selectedAlgorithm.isEmpty()) {
                         try {
-                            if (selectedAlgorithm.equalsIgnoreCase("AES")) {
-                                // Generera AES-nyckel
-                                aesKey = EncryptionUtils.generateAESKey(Integer.parseInt(keyLength));
+                            int keyLength = Integer.parseInt(keyLengthStr);
 
-                                // Dekryptera med AES
+                            // Avkryptering med AES
+                            if (selectedAlgorithm.equalsIgnoreCase("AES")) {
                                 String decryptedText = EncryptionUtils.decryptAES(cipherText, aesKey);
-                                tV1.setText(decryptedText);
-                            } else if (selectedAlgorithm.equalsIgnoreCase("RSA")) {
-                                // Dekryptera med RSA
-                                String decryptedText = EncryptionUtils.decryptRSA(cipherText, privateKey);
-                                tV1.setText(decryptedText);
-                            } else {
-                                Toast.makeText(MainActivity.this, "Ogiltig/fel algoritm har valts", Toast.LENGTH_SHORT).show();
+                                tV1.setText("Decrypted Text: " + decryptedText);
                             }
+                            // Avkryptering med RSA
+                            else if (selectedAlgorithm.equalsIgnoreCase("RSA")) {
+                                if (keyLength == 128 || keyLength == 256) {
+                                    String decryptedText = EncryptionUtils.decryptRSA(cipherText, privateKey);
+                                    tV1.setText("Decrypted Text: " + decryptedText);
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Invalid RSA Key Length", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(MainActivity.this, "Invalid/Incorrect Algorithm selected", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (NumberFormatException e) {
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Invalid Key Length", Toast.LENGTH_SHORT).show();
                         } catch (Exception e) {
                             e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Decryption failed", Toast.LENGTH_SHORT).show();
                         }
                     } else {
-                        Toast.makeText(MainActivity.this, "Fyll i alla fälten", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Fill in all fields", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
@@ -116,3 +142,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+// Krpytering appen och koden är skriven av Ali Ferzali för Kursen Tillämpad Programmering på KAU.
